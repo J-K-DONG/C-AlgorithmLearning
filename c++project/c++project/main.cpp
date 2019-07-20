d0db3c75bbafbdbc68e410f66cacf75310ed95e5
J-K-DONG <dongjinkun99@gmail.com>
2019-07-20 18:55:48 +0800

    乘法溢出判断

--- c++project/c++project/main.cpp
+++ c++project/c++project/main.cpp
@@ -18,6 +18,20 @@ int tadd_ok(int x, int y){
     int pos_over = x > 0 && y > 0 && sum <= 0;//上溢出
     return !neg_over && !pos_over;
 }
+
+//乘法溢出检验
+int tmult_ok_div(int x, int y){
+    int p = x*y;
+    /*either x is zero, or dividing p by x gives y */
+    return !x || p/x == y;
+}
+
+int tmult_ok_int64(int x, int y){
+    int64_t pll = (int64_t) x*y; //强制类型转化 很关键
+    /*see if casting to int preserves value */
+    return pll == (int) pll;
+}
+
 int main(int argc, const char * argv[]) {
     int result;
     result = tadd_ok(-1, -2147483648); // -2147483648 int型最小值 保证溢出
